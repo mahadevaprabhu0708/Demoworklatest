24a4edf11ff5ab077e1754db80872dec59dd92e7
mahadg <mahadg@USBLRMAHADG2.us.deloitte.com>
2024-11-13 09:12:18 +0530

    latest

---
+++ demoworknewonel/src/test/java/ToCreateContactTest/ToCreateContact1Test.java
@@ -0,0 +1,10 @@
+package ToCreateContactTest;
+
+public class ToCreateContact1Test {
+
+	public static void main(String[] args) {
+		System.out.println("Success");
+
+	}
+
+}
---
+++ demoworknewonel/src/test/java/ToCreateOrgTest/ToCreateOrg1Test.java
@@ -0,0 +1,10 @@
+package ToCreateOrgTest;
+
+public class ToCreateOrg1Test {
+
+	public static void main(String[] args) {
+		System.out.println("success");
+
+	}
+
+}
